6d53a0c02e976793a24b093f67fcb867dfd834ec
Dhanushiya13 <139323240+Dhanushiya13@users.noreply.github.com>
2025-04-26 16:07:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// (optional) for smooth scroll behavior
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
